2391f3930e456bbfce61f55fac9275867a5d3802
Sree-Ved <71084743+Sree-Ved@users.noreply.github.com>
2020-10-05 19:34:20 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# bounce-Off-algorithnm
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,37 @@
+var fixedRect, movingRect;
+// initialising rectangles
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(400, 800, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  fixedRect.velocityY = -7;
+  movingRect = createSprite(400,100,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+  movingRect.velocityY = 7; 
+}
+
+function draw() {
+  background(0,0,0);  
+  //movingRect.x = World.mouseX;
+  //movingRect.y = World.mouseY;
+
+  bounceOff(movingRect,fixedRect);
+
+  drawSprites();
+}
+
+function bounceOff (object1,object2){  
+  if (object1.x - object2.x < object2.width/2 + object1.width/2
+      && object2.x - object1.x < object2.width/2 + object1.width/2){
+      object1.velocityX = object1.velocityX * (-1);
+      object2.velocityX = object2.velocityX * (-1);
+
+      }
+  if( object1.y - object2.y < object2.height/2 + object1.height/2
+      && object2.y - object1.y < object2.height/2 + object1.height/2) {
+      object1.velocityY = object1.velocityY * (-1);
+      object2.velocityY = object2.velocityY * (-1);
+    }} 
